9e7c3a23407bfeede4a3d29bc427c3d35129b75a
jelmer-agricola <jelmer.agricola@gmail.com>
2022-12-06 09:50:24 +0100

    structuur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
+      <>
     <div>
       Begin hier met de opdracht!
     </div>
+          <button type="button">Vorige</button>
+          <button type="button">Volgende</button>
+      </>
   );
 }
 
